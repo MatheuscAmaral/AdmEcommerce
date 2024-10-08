b4c26ce41e07aabc53477c455427f842b5916bae
MatheuscAmaral <matheuscamposdoamaral15@gmail.com>
2024-10-08 11:26:31 -0300

    verify if is local or not

--- api/index.tsx
+++ api/index.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const hostName = window.location.hostname;
+const isLocal = typeof window !== 'undefined' && window.location.hostname === "localhost";
 
 const api = axios.create({
-    baseURL: `${hostName === "localhost" ? "http://localhost:3333" : "https://rwa-api-2-0.vercel.app"}`
+    baseURL: isLocal ? "http://localhost:3333" : "https://rwa-api-2-0.vercel.app"
 });
 
 export default api;
