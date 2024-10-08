fb11c006a81cb6afac6679ac40c7e19f61cbca10
MatheuscAmaral <matheuscamposdoamaral15@gmail.com>
2024-10-08 11:10:20 -0300

    url api prod

--- api/index.tsx
+++ api/index.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:3333"
-})
+const hostName = window.location.hostname;
 
+const api = axios.create({
+    baseURL: `${hostName == "localhost" ? "http://localhost:3333" : "https://rwa-api-2-0.vercel.app"}`
+});
 export default api;
