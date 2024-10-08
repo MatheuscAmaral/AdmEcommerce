8faafc890d40a4c215ed934a59f99a33eb40c6b3
MatheuscAmaral <matheuscamposdoamaral15@gmail.com>
2024-10-08 11:22:44 -0300

    hostname window

--- api/index.tsx
+++ api/index.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const hostName = window.location.hostname;
 
 const api = axios.create({
-    baseURL: `${hostName == "localhost" ? "http://localhost:3333" : "https://rwa-api-2-0.vercel.app"}`
+    baseURL: `${hostName === "localhost" ? "http://localhost:3333" : "https://rwa-api-2-0.vercel.app"}`
 });
+
 export default api;
